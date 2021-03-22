6125e9d8bf588b0ee47f887ea494f72b56514928
satsukizzz <dn.asp01@gmail.com>
2021-03-23 00:12:51 +0900

    show GUI window to notice warning

--- check.py
+++ check.py
@@ -2,12 +2,13 @@ import subprocess
 
 # local files
 import settings as env
+import warn
 
 try:
   res = subprocess.run(["powershell.exe", "ps", "|", "wsl", "grep", env.a_game_name, "|", "wsl", "wc", "-l"], stdout=subprocess.PIPE)
 
   if(res.stdout == "1\n".encode('utf-8')):
-    exec(open("./warn.py").read())
+    warn.show_warning("that")
   else:
     print("going well.")
 
--- warn.py
+++ warn.py
@@ -1,1 +1,16 @@
-print("why are you playing now?")
+import tkinter as tk
+from tkinter import font
+
+def show_warning(game_title):
+  main_view = tk.Tk()
+  main_view.title("not the time to play.") 
+  main_view.geometry("500x50")
+
+  #これじゃ上手くいかない
+  # my_font = tk.font.Font(main_view, family="Segoe UI", size=16)
+  my_font = font.Font(main_view, family="Segoe UI", size=16)
+
+  label = tk.Label(main_view, text="you must notice it's not the time to play " + game_title, font=my_font)
+  label.pack()
+
+  main_view.mainloop()
