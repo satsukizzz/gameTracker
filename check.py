a906ba41a135a26f4b30668fec832425785f7f9a
satsukizzz <dn.asp01@gmail.com>
2021-03-22 23:33:37 +0900

    fix: exception for executing shell command

--- check.py
+++ check.py
@@ -6,7 +6,10 @@ import settings as env
 try:
   res = subprocess.run(["powershell.exe", "ps", "|", "wsl", "grep", env.a_game_name, "|", "wsl", "wc", "-l"], stdout=subprocess.PIPE)
 
-if(res.stdout == "1\n".encode('utf-8')):
-  exec(open("./warn.py").read())
-else:
-  print("going well.")
+  if(res.stdout == "1\n".encode('utf-8')):
+    exec(open("./warn.py").read())
+  else:
+    print("going well.")
+
+except:
+  print(__file__ + " errored")
